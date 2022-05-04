90e5ed411ebf2aaa837c9a2933f39df4ba796679
hjc2000 <huangjincan163@163.com>
2022-05-04 16:28:37 +0800

    1

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,22 @@
 #include <Arduino.h>
 #include <stdint.h>
 #include "MspTar.h"
+#include "DallasTemperature.h"
+OneWire oneWire(P1_4);
+DallasTemperature sensors(&oneWire);
 
 void setup()
 {
   Serial.begin(9600);
   pinMode(RED_LED, OUTPUT);
   new MspTar(&Serial);
+  sensors.begin();
 }
 
 void loop()
 {
-  float temp = 20;
+  sensors.requestTemperatures();
+  float temp = sensors.getTempCByIndex(0);
   pTar->sendData(2, (uint8_t *)&temp, sizeof(temp));
   pTar->loop();
   delay(1000);
