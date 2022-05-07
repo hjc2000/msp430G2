ebbf18c581c0e838a9d5c6047fe314590307134d
hjc2000 <huangjincan163@163.com>
2022-05-07 23:46:49 +0800

    1

--- src/MspTar.cpp
+++ src/MspTar.cpp
@@ -43,5 +43,30 @@ void MspTar::OnReceive(Queue<uint8_t> &data)
         }
         break;
     }
+    case 2: //给定值
+    {
+        if (data.pop())
+        {
+            //设置给定值
+        }
+        else
+        {
+            //读取给定值
+        }
+        break;
+    }
+    case 3: // PID参数
+    {
+        if (data.pop())
+        {
+            //写PID参数
+            /*PID参数为3个double类型的值，从队列中取出3个double*/
+        }
+        else
+        {
+            //读PID参数
+        }
+        break;
+    }
     }
 }
